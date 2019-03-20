214351fdf7eafd42971b8c8c28f4b8d2c0287c52
melody <you@example.com>
2019-03-20 19:40:35 +0800

    v0.0.1 - finish the web frame

---
+++ js/subpagehead.js
@@ -0,0 +1,32 @@
+// document.writeln("<head>");
+document.writeln("    <meta charset=\'utf-8\'>");
+document.writeln("    <meta http-equiv=\'X-UA-Compatible\' content=\'IE=edge\'>");
+// document.writeln("    <title>Go语言-Melody House</title>");
+document.writeln("    <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'>");
+document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'../css/clearstyle.css\' />");
+document.writeln("    <link rel=\'shortcut icon\' href=\'../img/favicon.png\'>");
+document.writeln("    <link rel=\'bookmark\' href=\'../img/favicon.png\'>");
+document.writeln("    <link id=\'bootstrap_221\' rel=\'stylesheet\' type=\'text/css\' class=\'library\' href=\'../plugin/bootstrap-2.2.1/css/bootstrap.min.css\'>");
+document.writeln("    <script id=\'jquery_172\' type=\'text/javascript\' class=\'library\' src=\'../plugin/jquery-3.2.1/jquery.min.js\'></script>");
+document.writeln("    <script id=\'bootstrap_221\' type=\'text/javascript\' class=\'library\' src=\'../plugin/bootstrap-2.2.1/js/bootstrap.min.js\'></script>");
+document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'../css/subpage.css\' />");
+document.writeln("    <link rel=\'stylesheet\' type=\'text/css\' media=\'screen\' href=\'../css/artical.css\' />");
+// document.writeln("</head>");
+
+/*
+//<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Go语言-Melody House</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/clearstyle.css" />
+    <link rel="shortcut icon" href="../img/favicon.png">
+    <link rel="bookmark" href="../img/favicon.png">
+    <link id="bootstrap_221" rel="stylesheet" type="text/css" class="library" href="../plugin/bootstrap-2.2.1/css/bootstrap.min.css">
+    <script id="jquery_172" type="text/javascript" class="library" src="../plugin/jquery-3.2.1/jquery.min.js"></script>
+    <script id="bootstrap_221" type="text/javascript" class="library" src="../plugin/bootstrap-2.2.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/subpage.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/artical.css" />
+//</head>
+
+*/
